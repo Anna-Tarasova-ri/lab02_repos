d160bcc74aabe5a125e0d4c63fd0fc2434e4988a
Anna Tarasova <anna.tarasova.ri.2026@lpnu.ua>
2026-09-19 23:15:36 +0300

    закоментовано 16-ий, 21-ий, 25-ий рядки

--- pr2.cpp
+++ pr2.cpp
@@ -13,16 +13,16 @@ int main()
 	double Pi = 4 * atan(1.); // число пі
 	double alpha; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	//double z2; // результат обчислення 2-го виразу
 	
 	cout << "alpha = "; cin >> alpha;
 	
 	z1 = pow(cos(3.0 * Pi / 8.0 - alpha / 4.0), 2) - pow(cos(11.0 * Pi / 8.0 + alpha / 4.0), 2);
-	z2 = (sqrt(2.0) / 2.0) * sin(alpha / 2.0);
+	//z2 = (sqrt(2.0) / 2.0) * sin(alpha / 2.0);
 	
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 	
 	cin.get();
 	return 0;
